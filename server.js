8066e3c09d4b78af8581778cd376c4dd8f0f9ce8
LAPTOP-EIS41EO7\Aacas <aacastellanosc1@outlook.com>
2025-06-26 09:13:43 +0200

    improve code

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Servidor seguro en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
