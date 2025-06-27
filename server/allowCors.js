5b041e9f94fd4b7cb437db7baf452b874b3037e2
LAPTOP-EIS41EO7\Aacas <aacastellanosc1@outlook.com>
2025-06-27 19:37:32 +0200

    allow cors on production

--- server/allowCors.js
+++ server/allowCors.js
@@ -1,6 +1,15 @@
 // Permite CORS para desarrollo entre http://localhost:3000 y http://localhost:3002
 module.exports = function allowCors(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3002',
+    'https://livecoder-wtbx.onrender.com',
+    'https://www.livecoder-wtbx.onrender.com'
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
